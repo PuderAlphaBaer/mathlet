9ea183364186d6be0b73805e0e06743ad5be8527
Chriveloper <111074541+PuderAlphaBaer@users.noreply.github.com>
2024-12-07 20:02:25 +0100

    add Home component and route to App; update navigation structure

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,14 @@ import Foerderung from './pages/Foerderung';
 import Materialien from './pages/Materialien.tsx';
 import KursleiterWerden from './pages/KursleiterWerden.tsx';
 import UeberUns from './pages/UeberUns.tsx';
+import Home from './pages/Home'; // Import the new Home component
 
 const App: React.FC = () => {
   return (
     <Router>
       <Navbar />
       <Routes>
+        <Route path="/" element={<Home />} /> {/* Add the new route */}
         <Route path="/ankuendigungen" element={<Ankuendigungen />} />
         <Route path="/foerderung" element={<Foerderung />} />
         <Route path="/materialien" element={<Materialien />} />
